6247d083acd2d2c8b546b72da1d3520a70d8b340
Maxo <mmaxim@smartfort.org>
2018-02-08 15:03:17 +0300

    web

--- src/Dao.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-import entities.Student;
-import java.util.*;
-import java.sql.SQLException;
-
-public interface Dao{
-	public List <Student> selectAll() throws SQLException;
-
-}
